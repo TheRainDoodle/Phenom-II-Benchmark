6814280c09b2bb1915cb95c28b956cd607ecc985
FelixCLC (FCLC) <felix.leclair123@hotmail.com>
2022-02-10 12:42:05 -0500

    add AVX512 functionality
    Performance metrics on AVX-512 enabled alderlake:
    
    8 P-cores/16threads of AVX-512 :
    
    Run 1/10 Time: 0.08902
    Run 2/10 Time: 0.08652
    Run 3/10 Time: 0.08655
    Run 4/10 Time: 0.0866
    Run 5/10 Time: 0.08652
    Run 6/10 Time: 0.08653
    Run 7/10 Time: 0.08604
    Run 8/10 Time: 0.08657
    Run 9/10 Time: 0.08641
    Run 10/10 Time: 0.08656
    Executed 1248 billion instructions/second
    Score: 30.17 Phenom's II's worth's
    
    Single P-core/Single thread:
    
    Run 1/10 Time: 0.6904
    Run 2/10 Time: 0.6853
    Run 3/10 Time: 0.6853
    Run 4/10 Time: 0.6853
    Run 5/10 Time: 0.6853
    Run 6/10 Time: 0.6853
    Run 7/10 Time: 0.6852
    Run 8/10 Time: 0.6853
    Run 9/10 Time: 0.6853
    Run 10/10 Time: 0.6852
    Executed 156.7 billion instructions/second
    Score: 15.11 Phenom's II's worth's

--- linux_src_bin/FLOPS.asm
+++ linux_src_bin/FLOPS.asm
@@ -1,5 +1,6 @@
 global FLOPS_SSE
 global FLOPS_AVX
+global FLOPS_AVX512
 
 section .text
 
@@ -25,6 +26,19 @@ section .text
 	vaddps ymm6, ymm6, ymm0
 	vaddps ymm7, ymm7, ymm0
 	vaddps ymm8, ymm8, ymm0
+%endmacro;
+
+
+; For AVX512 capable CPU's
+%macro ADD_AVX512 0
+	vaddps zmm1, zmm1, zmm0
+	vaddps zmm2, zmm2, zmm0
+	vaddps zmm3, zmm3, zmm0
+	vaddps zmm4, zmm4, zmm0
+	vaddps zmm5, zmm5, zmm0
+	vaddps zmm6, zmm6, zmm0
+	vaddps zmm7, zmm7, zmm0
+	vaddps zmm8, zmm8, zmm0
 %endmacro
 
 FLOPS_SSE:
@@ -114,6 +128,64 @@ FLOPS_AVX:
 	ADD_AVX
 	ADD_AVX
 	ADD_AVX
+
+	dec r12
+	jnz .LoopHead
+
+	pop r12
+	pop r10
+	pop r9
+	pop r8
+	pop r11
+	pop rbp
+	pop rsi
+	pop rdi
+	pop rbx
+	ret
+
+
+
+FLOPS_AVX512:
+	push rbx
+	push rdi
+	push rsi
+	push rbp
+	push r11
+	push r8
+	push r9
+	push r10
+	push r12
+
+	mov r12, (1024*1024*1024)/(128 * 16)		; x16 for AVX512
+
+	; Set all the AVX512 regs to 0.0
+	vxorps zmm0, zmm0, zmm0
+	vxorps zmm1, zmm1, zmm1
+	vxorps zmm2, zmm2, zmm2
+	vxorps zmm3, zmm3, zmm3
+	vxorps zmm4, zmm4, zmm4
+	vxorps zmm5, zmm5, zmm5
+	vxorps zmm6, zmm6, zmm6
+	vxorps zmm7, zmm7, zmm7
+	vxorps zmm8, zmm8, zmm8
+
+.LoopHead:
+	ADD_AVX512
+	ADD_AVX512
+	ADD_AVX512
+	ADD_AVX512
+	ADD_AVX512
+	ADD_AVX512
+	ADD_AVX512
+	ADD_AVX512
+	ADD_AVX512
+	ADD_AVX512
+	ADD_AVX512
+	ADD_AVX512
+	ADD_AVX512
+	ADD_AVX512
+	ADD_AVX512
+	ADD_AVX512
 
 	dec r12
 	jnz .LoopHead
